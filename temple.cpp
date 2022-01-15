3172662e0a9c42bfb6063d5f05971b77cd387a59
ujjwal-shekhar <95298765+ujjwal-shekhar@users.noreply.github.com>
2022-01-15 17:47:51 +0530

    Create temple.cpp

---
+++ temple.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+/***********************************************************************/
+#define gc getchar_unlocked
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1) //for loop from k to n in correct order
+#define ll long long
+#define si(x)	scanf("%d",&x)        //scan int
+#define sl(x)	scanf("%lld",&x)      //scan long long
+#define ss(s)	scanf("%s",s)         //scan string
+#define pi(x)	printf("%d\n",x)      //print int
+#define pl(x)	printf("%lld\n",x)    //print long long
+#define ps(s)	printf("%s\n",s)      //print string
+/***********************************************************************/
+
+
+
+
+/***********************************************************************/
+#define deb(x) cout << #x << "=" << x << endl  //prints /name of x/ = /value of x/
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl 
+
+#define pb push_back
+#define mp make_pair
+
+#define F first
+#define S second
+
+#define all(x) x.begin(), x.end()
+
+#define clr(x) memset(x, 0, sizeof(x))
+
+#define sortall(x) sort(all(x))
+
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+/***********************************************************************/
+
+
+
+
+/***********************************************************************/
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;    //pair of two integers
+typedef pair<ll, ll>	pll;    //pair of two long long
+typedef vector<int>		vi;     // int vector
+typedef vector<ll>		vll;    // long long vector
+typedef vector<pii>		vpii;   // vector of pair of int int
+typedef vector<pll>		vpll;   //vector of pair of long long, long long
+typedef vector<vi>		vvi;    //2d vector ov int
+typedef vector<vll>		vvll;   //2d vector of long long
+/***********************************************************************/
+
+
+
+
+/***********************************************************************/
+//mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+//int rng(int lim) {
+//    uniform_int_distribution<int> uid(0,lim-1);
+//    return uid(rang);
+//}
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+//=======================
+
+
+
+
+
+
+void solve() {
+    int i, j, n, m;
+    
+
+}///
+
+
+
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+int main() {
+    //ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    //srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+
+    return 0;
+}
