85c24de228cc7bd19ca6e2dac87f6fda3f93be41
ujjwal-shekhar <95298765+ujjwal-shekhar@users.noreply.github.com>
2022-01-16 15:00:35 +0530

    Create halwa.cpp

---
+++ halwa.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define tr(container, it) for (typeof (container.begin()) it = container.begin(); it != container.end(); it++)
+
+
+int main()
+{
+    int n,k;
+
+    cin>>n>>k;
+
+
+
+
+
+/*
+    for(int i=0; i<=3*n; ++i)
+    {
+        string temp;
+        //scanf("%s", &temp);
+
+        getline(cin, temp);
+
+        M[temp]++;
+    }
+
+
+
+    
+    tr(M,it)
+    {
+        string iterer = it->first;
+        int iterar = it->second;
+
+        cout<<iterer << " : "<<iterar<<"\n";
+    }
+
+    //sort(M.begin(),M.end(), )
+
+    //cout<<endl;
+*/
+    return 0;
+}
